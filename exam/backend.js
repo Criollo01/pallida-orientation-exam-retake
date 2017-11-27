0060672ca080fea16b3a3fcab83bd63599f62c51
Criollo01 <lucanagy@protonmail.com>
2017-11-27 15:40:27 +0100

    feat(backend): changed database to the one with the correct numbers

--- exam/backend.js
+++ exam/backend.js
@@ -7,32 +7,38 @@ let mysql = require('mysql');
 app.use(express.static(__dirname + '/assets'));
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'a',
-    database: 'new'
+  host: 'localhost',
+  user: 'root',
+  password: 'a',
+  database: 'correct'
 });
 
 connection.connect(function(err) {
-    if (err) {
-        console.log('Cannot connect to database' + err.toString());
-    } else {
-        console.log('Connection established');
-    };
+  if (err) {
+    console.log('Cannot connect to database' + err.toString());
+  } else {
+    console.log('Connection established');
+  };
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/assets/index.html');
+  res.sendFile(__dirname + '/assets/index.html');
 });
 
-app.get('/warehouse', function(req, res){
-    connection.query('SELECT * FROM warehouse WHERE category = "sweaters"', function(err, rows){
-        console.log(res);
-        res.send({
-            "result": "ok",
-            "data": rows
-        });
+app.get('/warehouse', function(req, res) {
+  connection.query('SELECT * FROM warehouse', function(err, rows){
+		if (err) {
+			console.log(err.toString());
+		};
+		res.send({
+      "result": "ok",
+      "clothes": rows
     });
+  });
+});
+
+app.get('/price-check', function(req, res) {
+	
 });
 
 app.listen(3000, () => console.log('server is ok'));
