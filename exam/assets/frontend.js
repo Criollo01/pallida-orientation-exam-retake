917dd5fa09301ba94789c42d2a4aaae42f0fa7a6
Criollo01 <lucanagy@protonmail.com>
2017-11-27 15:04:43 +0100

    feat(frontend): add table header to table element

--- exam/assets/frontend.js
+++ exam/assets/frontend.js
@@ -3,30 +3,39 @@
 const button = document.querySelector('button');
 
 function ajax(method, url, callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.onload = function(){
-        callback(JSON.parse(xhr.responseText));
-    };
-    xhr.send();
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = function(){
+    callback(JSON.parse(xhr.responseText));
+  };
+  xhr.send();
 };
 
 function createTable(response){
-    console.log(response);
-    let table = document.createElement('table');
-    document.querySelector('body').appendChild(table);
-    console.log(response.clothes);
-    for (let i = 0; i < response.clothes.length; i++){
-        let elements = `<td>${response.clothes[i].item_name}</td>
-                        <td>${response.clothes[i].manufacturer}</td>
-                        <td>${response.clothes[i].category}</td>
-                        <td>${response.clothes[i].size}</td>
-                        <td>${response.clothes[i].unit_price}</td>
-                        <td>${response.clothes[i].in_store}</td>`;
-        let tableRows = document.createElement('tr');
-        tableRows.innerHTML = elements;
-        table.appendChild(tableRows);
-    };
+  let tableHeader = `<table>
+                        <tr>
+                        	<th>Licence plate</th>
+                          <th>Brand</th>
+                          <th>Model</th>
+                        	<th>Color</th>
+													<th>Year</th>
+													<th>In store</th>					
+                        </tr>
+										</table>`
+	let table = document.createElement('table');	
+	table.innerHTML = tableHeader;
+  document.querySelector('body').appendChild(table);
+  for (let i = 0; i < response.clothes.length; i++){
+    let elements = `<td>${response.clothes[i].item_name}</td>
+                    <td>${response.clothes[i].manufacturer}</td>
+                    <td>${response.clothes[i].category}</td>
+                    <td>${response.clothes[i].size}</td>
+                    <td>${response.clothes[i].unit_price}</td>
+                    <td>${response.clothes[i].in_store}</td>`;
+    let tableRows = document.createElement('tr');
+    tableRows.innerHTML = elements;
+    table.appendChild(tableRows);
+  };
 };
 
 ajax('GET', '/warehouse', createTable);
